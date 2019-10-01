6323917c2baf65a58ed21acc5eaa005cae4f71e2
dracuten1 <tuyennguyenleminh@gmail.com>
2019-10-01 16:16:52 +0700

    add web build

---
+++ build/precache-manifest.67572c9d3f6d1cb48743d7f1ca1ee957.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2ff7673520411637443d2d2479a7b08",
+    "url": "/RefactoredCaro/index.html"
+  },
+  {
+    "revision": "b9c3b6a7947995dd7b54",
+    "url": "/RefactoredCaro/static/css/main.4b98e4b8.chunk.css"
+  },
+  {
+    "revision": "b86022e77590eb28b1e5",
+    "url": "/RefactoredCaro/static/js/2.dc6d6334.chunk.js"
+  },
+  {
+    "revision": "b9c3b6a7947995dd7b54",
+    "url": "/RefactoredCaro/static/js/main.2458a2a7.chunk.js"
+  },
+  {
+    "revision": "726a60843ea35fa3705b",
+    "url": "/RefactoredCaro/static/js/runtime~main.3d1320e8.js"
+  }
+]);
