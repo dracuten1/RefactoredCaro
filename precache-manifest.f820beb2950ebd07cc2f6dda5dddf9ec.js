8616dedc2fdee92d159da4bcddbef52629a35b79
dracuten1 <tuyennguyenleminh@gmail.com>
2019-10-02 13:49:55 +0700

    Updates

---
+++ precache-manifest.f820beb2950ebd07cc2f6dda5dddf9ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed6ec51bd79e225772a00a79726a342a",
+    "url": "/RefactoredCaro/index.html"
+  },
+  {
+    "revision": "b4914e0ce3bceaea04e8",
+    "url": "/RefactoredCaro/static/css/main.b8da47cb.chunk.css"
+  },
+  {
+    "revision": "b86022e77590eb28b1e5",
+    "url": "/RefactoredCaro/static/js/2.dc6d6334.chunk.js"
+  },
+  {
+    "revision": "b4914e0ce3bceaea04e8",
+    "url": "/RefactoredCaro/static/js/main.11205526.chunk.js"
+  },
+  {
+    "revision": "726a60843ea35fa3705b",
+    "url": "/RefactoredCaro/static/js/runtime~main.3d1320e8.js"
+  }
+]);
